ccc93747d068487297fd58304c485d3074f5a531
Harshita Tupili <harshita.tupili@gmail.com>
2019-02-01 14:27:07 -0500

    Update a1/starter.py

--- a1/starter.py
+++ a1/starter.py
@@ -23,26 +23,16 @@ def loadData():
 
 def MSE(W, b, x, y, reg):
     total_loss = 0
-    MSEloss=0
-    N = len(y[0]);
-	yhat=np.dot(np.transpose(W),x)
-	MSEloss = yhat-y.flatten()+ b #need to figure out how to do this
-	MSEloss=MSEloss **2
-	loss=np.sum(abs(MSEloss))
+    MSEloss = 0
+    N = len(x)
+    yhat = np.dot(np.transpose(W), x)
+    MSEloss = yhat-y.flatten()+ b #need to figure out how to do this
+    MSEloss=MSEloss ** 2
+    loss=np.sum(abs(MSEloss))
 
 
-	weight_decay_loss = (reg / 2) * np.sum(np.linalg.norm(np.dot(W,W)) ** 2) #sum the array
-	 total_loss = (loss / (2 * N) + weight_decay_loss
-
-    for i in range(0, N):
-
-	MSEloss += np.linalg.norm(yhat-y[i] +b))**2
-        #MSEloss = (1 / (2 * N)) * np.linalg.norm((W.T * x[i] + b + y[i])) ** 2
-
-
-    total_loss = (MSEloss / (2 * N) + weight_decay_loss
-
-
+    weight_decay_loss = (reg / 2) * np.sum(np.linalg.norm(np.dot(W,W)) ** 2) #sum the array
+    total_loss = (loss / (2 * N)) + weight_decay_loss
 
     return total_loss
 
@@ -72,6 +62,6 @@ def main():
     trainData, validData, testData, trainTarget, validTarget, testTarget = loadData()
     x, y = trainData, trainTarget
     W = np.zeros(x.shape)
-    print("hi")
+    print(len(x))
 
 main()
